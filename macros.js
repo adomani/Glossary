02d0dbedb00afd0ec085fa34d845347f05a0b95e
adomani <adomani@gmail.com>
2020-11-03 12:33:20 +0100

    new file:   2020_11_03_lavagna.jpeg 	new file:   index.html 	new file:   macros.js

---
+++ macros.js
@@ -0,0 +1,16 @@
+MathJax.Hub.Config({
+  TeX: {
+    Macros: {
+      C: "{\\mathbb C}",
+      Q: "{\\mathbb Q}",
+      Z: "{\\mathbb Z}",
+      spec: "{\\operatorname{Spec}\\,}",
+      oh: "{\\frac{1}{2}}",
+      zoh: "{\\Z [\\oh]}",
+      RR: "{\\bf R}",
+      bold: ["{\\bf #1}",1]
+    }
+  }
+});
+
+MathJax.Ajax.loadComplete("https://homepages.warwick.ac.uk/~maskal/glossary/macros.js");
